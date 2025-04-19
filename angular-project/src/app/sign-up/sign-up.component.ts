5aab19300c022c15d91f0e61539918ef34a4b1ed
Vinícius Caixeta de Souza <62401765+vinis-caixe@users.noreply.github.com>
2025-04-19 01:26:19 +0000

    add form for inputs

--- angular-project/src/app/sign-up/sign-up.component.ts
+++ angular-project/src/app/sign-up/sign-up.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-up.component.css'],
 })
 export class SignUpComponent implements OnInit {
+  username = '';
+  email = '';
   password = '';
   passwordRepeat = '';
 
@@ -13,12 +15,34 @@ export class SignUpComponent implements OnInit {
 
   ngOnInit(): void {}
 
+  onChangeUsername(event: Event) {
+    this.username = (event.target as HTMLInputElement).value;
+  }
+
+  onChangeEmail(event: Event) {
+    this.email = (event.target as HTMLInputElement).value;
+  }
+
   onChangePassword(event: Event) {
     this.password = (event.target as HTMLInputElement).value;
   }
 
   onChangePasswordRepeat(event: Event) {
     this.passwordRepeat = (event.target as HTMLInputElement).value;
+  }
+
+  onClickSignUp() {
+    fetch('/api/1.0/users', {
+      method: 'POST',
+      body: JSON.stringify({
+        username: this.username,
+        password: this.password,
+        email: this.email,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
   }
 
   isDisabled() {
